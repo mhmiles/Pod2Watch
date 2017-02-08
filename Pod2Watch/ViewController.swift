c48d43604be938303fe897fa3337b966f614e69c
Miles Hollingsworth <mhmiles@me.com>
2017-02-08 09:53:07 -0800

    Initial Commit

---
+++ Pod2Watch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pod2Watch
+//
+//  Created by Miles Hollingsworth on 2/8/17.
+//  Copyright © 2017 Miles Hollingsworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
